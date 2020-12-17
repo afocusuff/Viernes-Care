81d5f1dc889fae685c2f5e010d9998403f66f5eb
khalifa boulbayem <khalo_2@hotmail.com>
2020-12-17 11:50:55 +0000

    modifcando archivo  sql

--- BBDD/paciente.sql
+++ BBDD/paciente.sql
@@ -6,7 +6,7 @@
 -- Tiempo de generación: 17-12-2020 a las 10:22:39
 -- Versión del servidor: 5.7.24
 -- Versión de PHP: 7.2.19
-
+--base de tados 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
